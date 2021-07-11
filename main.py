ecccaf408619ab649ba96ec0d53d8799dea53ff5
lennibot <Kitcar8996>
2021-07-11 16:15:01 -0400

    Bug Fixes #3

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import time
 import signal
 import OrangeSherbet
 
-orange = OrangeSherbet
-
 if __name__ == '__main__':
     try:
         """
         At the moment all this script does it check, update, and verify the current version of Paper.
         Eventually this main script will spawn several subprocesses that handle the various needs of a Minecraft Server.
         """
+        orange = OrangeSherbet
+
         orange.check_version()
 
     except KeyboardInterrupt:

3d2f002138d5116f347586561a5082280a23ea57
eric-gore <ericgore@yahoo.com>
2021-07-13 10:40:19 -0400

    Update Install Script

--- tools/install.sh
+++ tools/install.sh
@@ -22,7 +22,7 @@ main() {
     fi
 
     # Check python version installed
-    printf "${BLUE}Checking python install...${NORMAL}\n"
+    printf "${GREEN}Checking python install...${NORMAL}\n"
     req=`python3 -c 'import sys; exit(1) if sys.version_info.major < 3 and sys.version_info.minor < 8 else exit(0)'`
     if [[ $req = 0 ]]; then
         printf "Python version 3.8 or higher is required."
@@ -30,7 +30,7 @@ main() {
         printf "${GREEN}Python installed...${NORMAL}\n"
     fi
 
-    printf "${YELLOW}Cloning Orange Sherbet${NORMAL}\n"
+    printf "${BLUE}Cloning Envelope${NORMAL}\n"
     hash git >/dev/null 2>&1 || {
         echo "ERROR: Please install git to use this utility\n"
         exit 1
@@ -41,22 +41,22 @@ main() {
         exit 1
     }
 
-    printf "${BLUE}Creating Python virtual-enviroment${NORMAL}\n"
+    printf "${GREEN}Creating Python virtual-enviroment${NORMAL}\n"
     cd envelope/ || {
+      exit 1
     }
     python3 -m venv venv || {
         printf "Creating of virtual-environment failed...\n"
         exit 1
     }
 
-    printf "${BLUE}Installing required packages..${NORMAL}\n"
+    printf "${GREEN}Installing required packages..${NORMAL}\n"
     source venv/bin/activate
     pip install -r requirements.txt || {
         printf "installation of python packages failed...\n"
     }
 
-    printf "${YELLOW}${BOLD}Orange Sherbet sucessfully installed!${NORMAL}\n"
-
-} 
+    printf "${BLUE}${BOLD}Envelope successfully installed!${NORMAL}\n"
+}
 
 main
